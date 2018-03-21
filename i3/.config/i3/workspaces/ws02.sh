537c3db58d69613b76cb6449bb5a8be3a251b059
Jesper Fussing Moerk <jfm@moerks.dk>
2018-03-21 08:03:33 +0100

    Fixed chrome workspaces so that they always create a new window

--- i3/.config/i3/workspaces/ws02.sh
+++ i3/.config/i3/workspaces/ws02.sh
@@ -2,6 +2,6 @@
 . ~/.config/i3/env
 
 i3-msg "workspace 2: Web; append_layout ~/.config/i3/layouts/workspace2-web.json"
-i3-msg "workspace 2: Web; exec google-chrome-beta https://www.toggl.com/app/timer https://inbox.google.com/"
+i3-msg "workspace 2: Web; exec google-chrome-beta --new-window https://www.toggl.com/app/timer https://inbox.google.com/"
 i3-msg "workspace 2: Web; move workspace to output $PRIMARY"
 
