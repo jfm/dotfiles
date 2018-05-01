2d9733a71792562808f523a7eff04cfd3ecf3c4f
jfm <jfm@moerks.dk>
2018-05-01 08:35:12 +0200

    Trying to add proxy and auto start of mopidy

--- i3/.i3/workspaces/ws01.sh
+++ i3/.i3/workspaces/ws01.sh
@@ -2,5 +2,5 @@
 . ~/.i3/env
 
 i3-msg "workspace 1:Comms; append_layout ~/.i3/layouts/workspace1-comms.json"
-i3-msg "workspace 1:Comms; exec google-chrome-stable --no-default-browser-check --new-window https://chat.google.com https://mattermost.yousee.dk https://tdcgroup.slack.com"
+i3-msg "workspace 1:Comms; exec google-chrome-stable --proxy-pac-url=file://home/jfm/Customers/TDC/Scripts/tdcproxy.js --no-default-browser-check --new-window https://chat.google.com https://mattermost.yousee.dk https://tdcgroup.slack.com"
 i3-msg "workspace 1:Comms; move workspace to output $SECONDARY"
