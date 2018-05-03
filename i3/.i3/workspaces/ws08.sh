fef8875ec724436f739a0ad4c8d71f958b295863
jfm <jfm@moerks.dk>
2018-05-03 08:43:36 +0200

    Fixed media layout and startup

--- i3/.i3/workspaces/ws08.sh
+++ i3/.i3/workspaces/ws08.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 . ~/.i3/env
 i3-msg "workspace 8:Media; append_layout ~/.i3/layouts/workspace8-media.json"
-i3-msg "workspace 8:Media; exec urxvt -name tasks"
-i3-msg "workspace 8:Media; exec urxvt -name newsbeuter -e newsbeuter"
+i3-msg "workspace 8:Media; exec urxvt -name newsboat -e newsboat"
 i3-msg "workspace 8:Media; exec urxvt -name ncmpcpp -e ncmpcpp"
 i3-msg "workspace 8:Media; move workspace to output $LAPTOP"
