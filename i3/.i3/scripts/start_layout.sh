34a7fbea0ffe27292d15c9c646d42f413b07cc9d
jfm <jfm@moerks.dk>
2018-05-09 12:33:08 +0200

    added better bash configuration

--- i3/.i3/scripts/start_layout.sh
+++ i3/.i3/scripts/start_layout.sh
@@ -3,7 +3,7 @@
 CONNECTED_CMD="xrandr --query | grep connected | grep -v disconnected | wc -l"
 monitors=`eval $CONNECTED_CMD`
 
-echo $monitors
+sleep 5
 if [ $monitors -gt 1 ]
 then
 	~/.i3/scripts/dock.sh &
