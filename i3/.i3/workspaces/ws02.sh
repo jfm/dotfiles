3c87f39bf079cf284af7d0381afec4e8cefea744
jfm <jfm@moerks.dk>
2018-05-28 13:05:16 +0200

    Removed ncmpcpp on media WS

--- i3/.i3/workspaces/ws02.sh
+++ i3/.i3/workspaces/ws02.sh
@@ -2,6 +2,6 @@
 . ~/.i3/env
 
 i3-msg "workspace 2:Web; append_layout ~/.i3/layouts/workspace2-web.json"
-i3-msg "workspace 2:Web; exec google-chrome-stable --no-default-browser-check --new-window https://www.toggl.com/app/timer https://inbox.google.com/"
+i3-msg "workspace 2:Web; exec google-chrome-stable --no-default-browser-check --new-window https://www.toggl.com/app/timer https://mail.google.com/mail/u/0/#inbox"
 i3-msg "workspace 2:Web; move workspace to output $PRIMARY"
 
