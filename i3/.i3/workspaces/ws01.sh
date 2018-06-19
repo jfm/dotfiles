a5e36e64e59bf636d78b0a8154afb9332827ab0b
jfm <jfm@moerks.dk>
2018-06-19 20:51:49 +0200

    Adding messages and removing workspace

--- i3/.i3/workspaces/ws01.sh
+++ i3/.i3/workspaces/ws01.sh
@@ -2,5 +2,5 @@
 . ~/.i3/env
 
 i3-msg "workspace 1:Comms; append_layout ~/.i3/layouts/workspace1-comms.json"
-i3-msg "workspace 1:Comms; exec google-chrome-stable --no-default-browser-check --new-window https://chat.google.com https://mattermost.yousee.dk https://tdcgroup.slack.com https://tdcgroup.facebook.com/"
+i3-msg "workspace 1:Comms; exec google-chrome-stable --no-default-browser-check --new-window https://chat.google.com https://mattermost.yousee.dk https://tdcgroup.slack.com https://messages.android.com"
 i3-msg "workspace 1:Comms; move workspace to output $SECONDARY"
