20ab9e376113ed45a232e1ed1667f1061d4307f7
Jesper Fussing Mørk <jfm@moerks.dk>
2018-06-29 11:08:30 +0200

    Updated some fonts and fixed ws04 to use termite

--- i3/.i3/workspaces/ws04.sh
+++ i3/.i3/workspaces/ws04.sh
@@ -2,7 +2,7 @@
 . ~/.i3/env
 
 i3-msg "workspace 4:Work; append_layout ~/.i3/layouts/workspace4-work.json"
-i3-msg "workspace 4:Work; exec urxvt -name vim -e vim"
-i3-msg "workspace 4:Work; exec urxvt -name shell1"
-i3-msg "workspace 4:Work; exec urxvt -name shell2"
+i3-msg "workspace 4:Work; exec termite -t vim -e vim"
+i3-msg "workspace 4:Work; exec termite -t shell1"
+i3-msg "workspace 4:Work; exec termite -t shell2"
 i3-msg "workspace 4:Work; move workspace to output $PRIMARY"
